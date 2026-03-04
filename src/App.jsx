250ee6d3f6ba8bcb2bd16fad758adc15d0e474c2
Britany Montoya <britay-mon>
2026-03-04 17:18:21 -0500

    Initial commit: maqueta React con componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/NavBar';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="app-wrapper">
-      <NavBar />
       <Home />
     </div>
   );
